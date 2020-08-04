12495d5c985bf8845132af69468579056a7e281f
loveagri <1044531744@qq.com>
2020-08-04 13:37:49 +0700

    good

--- test.sh
+++ test.sh
@@ -1,4 +1,13 @@
-$ImageId=''
-docker run -d --name backup -p 5999:5000  -e JWTSECRET=secret -e DB_USERNAME=todo -e DB_PASSWORD=adming -e DB_NAME=todoDB $ImageId
-docker run -d --name mern1 -p 5001:5000 -e JWTSECRET=secret -e DB_USERNAME=todo -e DB_PASSWORD=adming -e DB_NAME=todoDB $ImageId
-docker run -d --name mern2 -p 5002:5000 -e JWTSECRET=secret -e DB_USERNAME=todo -e DB_PASSWORD=adming -e DB_NAME=todoDB $ImageId
+#!/usr/bin/env bash
+
+
+
+
+#ImageId='e8982a06b7629914f243367ba45b36d77a3458e33b211176879b4148c'
+#docker run -d --name backup -p 5999:5000  -e JWTSECRET=secret -e DB_USERNAME=todo -e DB_PASSWORD=adming -e DB_NAME=todoDB 1abf6c47f756
+#docker run -d --name mern1 -p 5001:5000 -e JWTSECRET=secret -e DB_USERNAME=todo -e DB_PASSWORD=adming -e DB_NAME=todoDB 1abf6c47f756
+#docker run -d --name mern2 -p 5002:5000 -e JWTSECRET=secret -e DB_USERNAME=todo -e DB_PASSWORD=adming -e DB_NAME=todoDB 1abf6c47f756
+
+
+REPOSITORY          TAG                 IMAGE ID            CREATED             SIZE
+g6219700/mern       v2                  3afc8b335df6        27 minutes ago      703MB
--- update.sh
+++ update.sh
@@ -3,55 +3,54 @@
 LAST_IMAGE=`cat /home/fuhong_tang_china/.lastImage.txt`
 echo "read last version image info successfully"
 
-FULL_APP_NAME=$1
+HUB_APP=$1
+VERSION=$2
+FULL_IMAGE_NAME="$HUB_APP:$VERSION"
 
 if [[ $1 = '' ]];
 then
-  echo "nee a full version info"
+  echo "---------------nee a full version info---------------"
   exit
 fi
 
-if [[ $FULL_APP_NAME = $LAST_IMAGE ]];
+if [[ $FULL_IMAGE_NAME = $LAST_IMAGE ]];
 then
-  echo "last version is same with new new version, don't update"
+  echo "---------------last version is same with new new version, don't update---------------"
   exit
 fi
 
-buildLog=`docker pull $FULL_APP_NAME | grep 'Digest: sha256:' `
-echo 'image pull finished'
+docker pull $FULL_IMAGE_NAME
+echo '---------------image pull finished---------------'
 
-# example: buildLog="Digest: sha256:78600a7836b7dc69b15c8456ee5ab517d9cda2874c087eace8fc5b7f850b04b4"
-echo $buildLog
-
-ImageId=`echo $buildLog |  grep 'Digest: sha256:'|sed -r "s/Digest: sha256:(.*)/\1/g"`
+ImageId=`docker images | grep -E $HUB_APP | grep -E $VERSION | awk '{print $3}'`
 echo "image id: $ImageId"
 
 docker rm -f backup
-echo "old server backup removed"
+echo "---------------old server backup removed---------------"
 
 docker run -d --name backup -p 5999:5000  -e JWTSECRET=secret -e DB_USERNAME=todo -e DB_PASSWORD=adming -e DB_NAME=todoDB $ImageId
 echo "backup server started"
 
 docker rm -f mern1
-echo "server mern1 removed"
+echo "---------------server mern1 removed---------------"
 
 docker rm -f mern2
-echo "server mern2 removed"
+echo "---------------server mern2 removed---------------"
 
 docker run -d --name mern1 -p 5001:5000 -e JWTSECRET=secret -e DB_USERNAME=todo -e DB_PASSWORD=adming -e DB_NAME=todoDB $ImageId
-echo "server mern1 started"
+echo "---------------server mern1 started---------------"
 
 docker run -d --name mern2 -p 5002:5000 -e JWTSECRET=secret -e DB_USERNAME=todo -e DB_PASSWORD=adming -e DB_NAME=todoDB $ImageId
-echo "server mern2 started"
+echo "---------------server mern2 started---------------"
 
 docker rmi $LAST_IMAGE
-echo "old image removed"
+echo "---------------old image removed---------------"
 
-echo $FULL_APP_NAME > /home/fuhong_tang_china/.lastImage.txt
-echo "new image info written successfully"
+echo $FULL_IMAGE_NAME > /home/fuhong_tang_china/.lastImage.txt
+echo "---------------new image info written successfully---------------"
 
-echo "deploy finished"
-echo "new version is $FULL_APP_NAME"
+echo "---------------deploy finished---------------"
+echo "---------------new version is $FULL_IMAGE_NAME---------------"
 
 
 
